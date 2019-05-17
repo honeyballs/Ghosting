b2999fba1aed85dc47145eec3a12bd4a7745e3a7
Yannick Bals <yannick.bals@web.de>
2019-05-17 22:05:58 +0200

    Add file type check and some frontend info

--- public/main.js
+++ public/main.js
@@ -4,27 +4,51 @@ function setButtonEnabled(enabled) {
     document.getElementById("submit").disabled = !enabled
 }
 
-function setProgressBarVisible(visible) {
+function setFileInfoProgressVisibility(visible) {
     const prog = document.getElementsByTagName("progress")[0]
-    visible ? prog.classList.remove("invisible") : prog.classList.add("invisible")
+    const nameP = document.getElementById("filename")
+    if (visible) {
+        prog.classList.remove("invisible")
+        nameP.classList.remove("invisible")
+    } else {
+        prog.classList.add("invisible")
+        nameP.classList.add("invisible")
+    }
+}
+
+function setError(show, message) {
+    const err = document.getElementById("error")
+    if (show) {
+        err.classList.remove("invisible")
+    } else {
+        err.classList.add("invisible")
+        err.innerText = ""
+    }
 }
 
 function setProgress(percent) {
     document.getElementsByTagName("progress")[0].value = percent
 }
 
-function fileChange() {    
+function fileChange() {
     const file = document.getElementById("zip").files[0];
-    console.log(file.type)
 
-    setButtonEnabled(true)
-    setProgress(0)
-    setProgressBarVisible(true)
+    if (file.type !== "application/zip") {
+        setError(true, "Please choose a .zip file.")
+    } else {
+        setError(false, "")
+        document.getElementById("filename").innerText = file.name
+        document.getElementsByTagName("label")[0].innerText = "Choose another file"
+        setButtonEnabled(true)
+        setProgress(0)
+        setFileInfoProgressVisibility(true)
+    }
+
 }
 
 function submitForm() {
     const file = document.getElementById("zip").files[0]
-    
+
     if (!file) {
         return
     }
@@ -36,7 +60,7 @@ function submitForm() {
     // Build the request
     const request = new XMLHttpRequest()
     request.onerror = err => console.log(err)
-    request.onprogress = prog => setProgress(Math.round(prog.total/prog.loaded * 100))
+    request.onprogress = prog => setProgress(Math.round(prog.total / prog.loaded * 100))
     request.onabort = () => console.log("Submit cancelled")
     request.onload = () => console.log(request.responseText)
 
